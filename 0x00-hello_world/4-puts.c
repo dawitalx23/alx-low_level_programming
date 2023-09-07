8e465963851c578d7e82e9d615e0215d797e00ac
dawitalx23 <dawitalx23@gmail.com>
2023-09-07 08:10:19 -0700

    Commit3: 4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Entry - point
+ * main - Prints a sentence to the standard output
  *
  * return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle4. Hello, puts");
 	return (0);
 }
