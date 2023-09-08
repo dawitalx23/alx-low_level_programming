4603621335c206a8b32c75998867487810927791
dawitalx23 <dawitalx23@gmail.com>
2023-09-08 10:45:35 -0700

    Commit2: 9. Patience, persis...

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,17 +8,16 @@
 int main(void)
 {
 	int i;
-	int j;
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j <= 9; j++)
+		putchar('0' + i);
+		if (i < 9)
 		{
-			putchar('0' + i);
-			putchar('0' + j);
 			putchar(',');
 			putchar(' ');
 		}
 	}
+	putchar('\n');
 	return (0);
 }
