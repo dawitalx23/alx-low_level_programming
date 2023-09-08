a452633a294432a99aa81c5571d57a09e16f2e57
dawitalx23 <dawitalx23@gmail.com>
2023-09-08 10:06:03 -0700

    Commit: 8. Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints all the numbers of base 16 in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	char c;
+
+	for (i = 0; i < 10; i++)
+		putchar('0' + i);
+	for (c = 'a'; c <= 'f'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
