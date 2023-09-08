54c88cfb6946fb2fb2159d002aac72f80432e22e
dawitalx23 <dawitalx23@gmail.com>
2023-09-08 10:39:04 -0700

    Commit: 9. Patience, per...

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of single-digit numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			putchar('0' + i);
+			putchar('0' + j);
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	return (0);
+}
