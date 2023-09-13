420fc87015e6dfd50393d5145c71093022031fa6
dawitalx23 <dawitalx23@gmail.com>
2023-09-13 12:41:36 -0700

    Commit: 12. The World loo...

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_times_table - prints the n times table, starting with 0.
+ * @n: argument
+ *
+ * Return: void
+ */
+void print_times_table(int n)
+{
+	int i = 0, j;
+
+	if (n < 0 || n > 15)
+		return;
+
+	while (i <= n)
+	{
+		j = 0;
+		while (j <= n)
+		{
+			if (i * j < 10 && j != 0)
+				printf("  ");
+			else if(i * j < 100 && i * j >= 10)
+				printf(" ");
+			if (j == n)
+				printf("%d\n", i * j);
+			else
+				printf("%d, ", i * j);
+			j++;
+		}
+		i++;
+	}
+}
