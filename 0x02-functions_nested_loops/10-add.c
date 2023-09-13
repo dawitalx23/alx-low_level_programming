cef13468518cfcd455d65ee2e33dc5f4728d5bd4
dawitalx23 <dawitalx23@gmail.com>
2023-09-13 11:03:22 -0700

    Commit: 10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @a: integer argument
+ * @b: integer argument
+ *
+ * Return: sum of two nums
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
