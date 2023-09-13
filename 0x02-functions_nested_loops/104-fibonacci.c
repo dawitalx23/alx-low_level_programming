78ed5ebfa74fa5b25b1a21fcb3ecd243b3224b5f
dawitalx23 <dawitalx23@gmail.com>
2023-09-13 14:26:09 -0700

    Commit: 16. In computer cla

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 50 Fibonacci numbers, starting with 1 and 2
+ *
+ * Return: Always 0 Success
+ */
+int main(void)
+{
+	unsigned long int num1 = 0, num2 = 1;
+	int counter = 0;
+
+	while (counter < 98)
+	{
+		num1 = num1 + num2;
+		num2 = num2 + num1;
+		if (counter < 96)
+			printf("%lu, %lu, ", num1, num2);
+		else
+			printf("%lu, %lu\n", num1, num2);
+		counter += 2;
+	}
+	return (0);
+}
