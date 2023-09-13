57cfe21f69e61e623af4b76392e6db741e9a704a
dawitalx23 <dawitalx23@gmail.com>
2023-09-13 11:35:45 -0700

    Commit3: 11. 98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,12 +9,14 @@
  */
 void print_to_98(int n)
 {
-	while (n <= 98)
-	{
+	do {
+		if (n != 98)
+			printf("%d, ", n);
+		if (n > 98)
+			n--;
+		else if (n < 98)
+			n++;
 		if (n == 98)
 			printf("%d\n", n);
-		else
-			printf("%d, ", n);
-		n++;
-	}
+	} while (n != 98);
 }
