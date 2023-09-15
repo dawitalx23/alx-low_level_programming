c6600c82617804a10a3f64a61e8584b9c4158c79
dawitalx23 <dawitalx23@gmail.com>
2023-09-15 07:53:18 -0700

    Commit: 9. Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - prints fizz buzz and numbers
+ *
+ * Return: Always 0 Successful
+ */
+
+int main(void)
+{
+	int i = 1;
+
+	for (; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 != 0)
+			printf("Fizz");
+		else if (i % 3 != 0 && i % 5 == 0)
+			printf("Buzz");
+		else if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+		else
+			printf("%d", i);
+		if (i != 100)
+			printf(" ");
+	}
+	printf("\n");
+}
