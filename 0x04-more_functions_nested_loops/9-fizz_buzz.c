90f58c7040b96e666d573fa412f666a6e860541b
dawitalx23 <dawitalx23@gmail.com>
2023-09-15 07:59:24 -0700

    Commit#2: 9. Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,15 +13,13 @@ int main(void)
 	for (; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 != 0)
-			printf("Fizz");
+			printf("Fizz ");
 		else if (i % 3 != 0 && i % 5 == 0)
-			printf("Buzz");
+			printf("Buzz ");
 		else if (i % 3 == 0 && i % 5 == 0)
-			printf("FizzBuzz");
+			printf("FizzBuzz ");
 		else
-			printf("%d", i);
-		if (i != 100)
-			printf(" ");
+			printf("%d ", i);
 	}
 	printf("\n");
 }
