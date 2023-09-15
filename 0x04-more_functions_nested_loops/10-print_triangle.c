bb4494487876b323174e98f743cc209b5824410f
dawitalx23 <dawitalx23@gmail.com>
2023-09-15 08:22:49 -0700

    Commit: 10. Triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle
+ * @size: triangle size
+ *
+ * Return: void
+ */
+
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = size - 1; j >= 0; j--)
+		{
+			if (j > i)
+				_putchar(' ');
+			else
+				_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
