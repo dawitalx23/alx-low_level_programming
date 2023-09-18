6060453b8098a34e4b5c18b267dd910fd616293a
dawitalx23 <dawitalx23@gmail.com>
2023-09-18 13:08:01 -0700

    Commit: 4. I can only go one w...

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse, followed by a new line
+ * @s: arg
+ *
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int i = 0, j;
+
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+
+	for (j = i - 1; j >= 0; j--)
+	{
+		_putchar(*(s + j));
+	}
+	_putchar('\n');
+}
