6f88742e58f6e71eaf5af8b9bb447cb73a09f1b4
dawitalx23 <dawitalx23@gmail.com>
2023-09-21 14:34:46 -0700

    Commit#100

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,24 @@
+/**
+ * rot13 - encodes a string using rot13.
+ * @s: arg
+ *
+ * Return: String
+ */
+
+char *rot13(char *s)
+{
+	int i = 0, j;
+
+	char *inputs = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *outputs = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (; s[i] != '\0'; i++)
+	{
+		for (j = 0; inputs[j] != '\0'; j++)
+		{
+			if (s[i] == inputs[j])
+				s[i] = outputs[j];
+		}
+	}
+	return (s);
+}
