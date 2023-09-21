b86429d289bb8dc06ccb1ffb35f738120740e5a0
dawitalx23 <dawitalx23@gmail.com>
2023-09-21 14:50:12 -0700

    Commit:101

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * print_number - prints an integer.
+ * @n: arg
+ *
+ * Return: void
+ */
+
+void print_number(int n)
+{
+	unsigned int num;
+
+	if (n < 0)
+		_putchar('-');
+
+	num = abs(n);
+
+	if (num / 10)
+		print_number(num / 10);
+
+	_putchar('0' + (num % 10));
+}
