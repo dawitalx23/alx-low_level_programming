06d9a51c8eab22f9f3fb9361074f4c7c91208039
dawitalx23 <dawitalx23@gmail.com>
2023-09-21 13:47:41 -0700

    Commit#4

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,18 +11,16 @@ char *cap_string(char *s)
 	char *seps = " \t\n,;.!?\"(){}";
 
 	if (s[0] <= 'z' && s[0] >= 'a')
-		s[0] = s[0] - ('A' - 'a');
+		s[0] = s[0] - ('a' - 'A');
 
 	while (s[i] != '\0')
 	{
-		j = 0;
-		while (seps[j] != '\0')
+		for (j = 0; seps[j] != '\0'; j++)
 		{
 			if (s[i - 1] == seps[j] && s[i] <= 'z' && s[i] >= 'a')
 			{
-				s[i] = s[i] + ('A' - 'a');
+				s[i] = s[i] - ('a' - 'A');
 			}
-			j++;
 		}
 		i++;
 	}
