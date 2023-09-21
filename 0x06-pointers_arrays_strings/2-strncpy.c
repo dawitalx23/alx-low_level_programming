bf3c856ba024176d75c03f541c105160adff2706
dawitalx23 <dawitalx23@gmail.com>
2023-09-21 11:05:14 -0700

    Commit: 2. strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string.
+ * @dest: destinination String
+ * @src: String arg
+ * @n: size to be copied
+ *
+ * Return: string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0, len = 0;
+
+	while (src[i] != '\0')
+	{
+		i++;
+	}
+
+	len = i > n ? n : i;
+
+	for (i = 0; i < len; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
