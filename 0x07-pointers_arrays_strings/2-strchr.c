90899f045fff8fd06b682194a89aed5dec72cadf
dawitalx23 <dawitalx23@gmail.com>
2023-09-25 09:08:33 -0700

    Commit:

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include <stddef.h>
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: arg1
+ * @c: arg2
+ *
+ * Return: char*
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (*(s + i))
+	{
+		if (*(s + i) == c)
+			return (s + i);
+		i++;
+	}
+	return (NULL);
+}
