c9927e1726390af80d40c788a367c72ca81bb5de
dawitalx23 <dawitalx23@gmail.com>
2023-09-25 08:42:16 -0700

    Commit: 1. memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies memory area
+ * @dest: arg1
+ * @src: arg2
+ * @n: arg3
+ *
+ * Return: dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		*dest++ = *src++;
+		i++;
+	}
+	return (dest);
+}
