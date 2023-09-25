6e88924d2ffbce45331afe62bfab4ee0df126254
dawitalx23 <dawitalx23@gmail.com>
2023-09-25 08:29:55 -0700

    Commit: 0. memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memset - fills memory with a constant byte
+ * @s: arg1
+ * @b: arg2
+ * @n: arg3
+ *
+ * Return: chars
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+	char *newChars = s; 
+
+	for (; i < n; i++)
+	{
+		newChars[i] = b;
+	}
+	return (newChars);
+}
