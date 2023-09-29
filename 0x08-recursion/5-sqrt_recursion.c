6b0e3eaa38f4dcd15bd50c8d090a0e1f7359471a
dawitalx23 <dawitalx23@gmail.com>
2023-09-29 13:14:15 -0700

    Commit: 5. Your subco...

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * sqrtRec - return sqrt
+ * @a: int
+ * @b: int
+ *
+ * Return: int
+ */
+
+int sqrtRec(int a, int b)
+{
+	if (b * b == a)
+	{
+		return (b);
+	}
+
+	if (b * b < a)
+	{
+		return (-1);
+	}
+
+	return (sqrtRec(a, b - 1));
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: arg
+ *
+ * Return: int
+ */
+
+int _sqrt_recursion(int n)
+{
+	return (sqrtRec(n, n / 2));
+}
