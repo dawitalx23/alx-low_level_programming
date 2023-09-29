89bf2a8d19ef82e54be761c95812b69e08c9f007
dawitalx23 <dawitalx23@gmail.com>
2023-09-29 12:34:54 -0700

    Commit: 2. Dreams feel re...

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: arg
+ *
+ * Return: int
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s)
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+
+	return (0);
+}
