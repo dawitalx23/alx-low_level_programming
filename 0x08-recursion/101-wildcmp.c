99d3b81ece64648e2ec240377ca2e29e0b9bb03e
dawitalx23 <dawitalx23@gmail.com>
2023-09-29 14:21:02 -0700

    Commit: 8. Inception. No...

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,34 @@
+/**
+ * wildcmp - compares two strings
+ * @s1: arg1
+ * @s2: arg2
+ *
+ * Return: 1 if same else 0
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 && *s2)
+	{
+		return (1);
+	}
+
+	if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+
+	if (*s2 == '*')
+	{
+		if (*s2 == '*' && *(s2 + 1) && *s1)
+		{
+			return (0);
+		}
+
+		if (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2))
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
