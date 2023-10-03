e3c8f91ae03420121d4fec8691bf76bfb42f8be4
dawitalx23 <dawitalx23@gmail.com>
2023-10-03 02:17:19 -0700

    Commit: 1. The woman who ha

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _strdup - duplicates a string
+ * @str: string arg
+ *
+ * Return: NULL if out of memory and str is null elase string
+ */
+
+char *_strdup(char *str)
+{
+	int i = 0, len;
+	char *dup;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (*(str + i) != '\0')
+	{
+		i++;
+	}
+
+	len = i;
+
+	dup = (char *) malloc(len * sizeof(char));
+
+	if (dup == NULL)
+		return (NULL);
+
+	for (i = 0; i < len; i++)
+	{
+		dup[i] = str[i];
+	}
+
+	return (dup);
+}
