6c4ecad65323a675d2ffe8f9bdf1beb5912c57fc
dawitalx23 <dawitalx23@gmail.com>
2023-10-06 13:39:24 -0700

    Commit

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: arg
+ *
+ * Return: void *
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *result;
+
+	result = malloc(b * sizeof(int));
+
+	if (result == NULL)
+	{
+		return (NULL);
+	}
+
+	return (result);
+}
---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+#include <stddef.h>
+
+void *malloc_checked(unsigned int b);
+
+#endif /* MAIN_H */
