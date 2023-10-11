0bc264a6efeedcbf633a018cff358bed840c5533
dawitalx23 <dawitalx23@gmail.com>
2023-10-11 13:35:04 -0700

    commit

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+/**
+ * int_index - searches for an integer.
+ * @array: arg1
+ * @size: arg2
+ * @cmp: arg3
+ *
+ * Return: int
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (size <= 0)
+		return (-1);
+
+	for (; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+		{
+			return (i);
+		}
+	}
+	return (-1);
+}
