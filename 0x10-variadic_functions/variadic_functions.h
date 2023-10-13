5445f796949cb24604ed065614bf50851fce2282
dawitalx23 <dawitalx23@gmail.com>
2023-10-13 08:03:58 -0700

    Commit#3-0

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,9 @@
 #ifndef VARDIADIC_FUNCTIONS_H
 #define VARDIADIC_FUNCTIONS_H
 
+#include <stdarg.h>
+#include <stdio.h>
+
 int sum_them_all(const unsigned int n, ...);
 
 #endif /* VARDIADIC_FUNCTIONS_H */
