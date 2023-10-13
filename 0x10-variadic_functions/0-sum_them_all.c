f5fad3e613d8df055dde4a1dd21985bf191fe96c
dawitalx23 <dawitalx23@gmail.com>
2023-10-13 07:57:59 -0700

    Commit-0

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - sum all the args
+ * @n: arg
+ *
+ * Return: int
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list valist;
+	unsigned int i = 0;
+	int sum = 0;
+
+	if (n == 0)
+	{
+		return (0);
+	}
+
+	va_start(valist, n);
+
+	for (; i < n; i++)
+	{
+		sum =+ va_arg(valist, int);
+	}
+
+	va_end(valist);
+
+	return (sum);
+}
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,6 @@
+#ifndef VARDIADIC_FUNCTIONS_H
+#define VARDIADIC_FUNCTIONS_H
+
+int sum_them_all(const unsigned int n, ...);
+
+#endif /* VARDIADIC_FUNCTIONS_H */
