f010ecf94d19da30745b205bc7e72e9849b50d43
dawitalx23 <dawitalx23@gmail.com>
2023-10-13 08:10:10 -0700

    Commit-1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+/**
+ * print_numbers - prints numbers, followed by a new line
+ * @separator: arg1
+ * @n: const
+ *
+ * Return: void
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list valist;
+	unsigned int i = 0;
+
+	va_start(valist, n);
+
+	for (; i < n; i++)
+	{
+		printf("%d", va_arg(valist, int));
+
+		if (separator != NULL && i < n - 1)
+		{
+			printf("%s", separator);
+		}
+	}
+
+	printf("\n");
+	va_end(valist);
+}
