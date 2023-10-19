1c16e9c0c74410b0505a26790b910af7971d42a5
dawitalx23 <dawitalx23@gmail.com>
2023-10-19 03:15:26 -0700

    Commit:

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,6 +2,15 @@
 #define LISTS_H
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
 
 int _putchar(char c);
 
