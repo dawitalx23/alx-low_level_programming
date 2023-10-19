1c40e90fdfc2e74a66c8576f48161713f258c92d
dawitalx23 <dawitalx23@gmail.com>
2023-10-19 03:36:26 -0700

    Commit: 2

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ * add_node - adds a new node at the beginning of a list_t list
+ * @head: arg1
+ * @str: arg2
+ *
+ * Return: 
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *newNode;
+	int length = 0;
+
+	while (str[length])
+	{
+		length++;
+	}
+
+	newNode = malloc(sizeof(list_t));
+
+	if (!newNode)
+	{
+		return (NULL);
+	}
+
+	newNode->str = strdup(str);
+	newNode->len = length;
+	newNode->next = *hesd;
+	*head = newNode;
+	return (*head);
+}
